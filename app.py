9f8e4cd54c4b741f443366658f4e88d9525ad936
or-mv <or.gamliel@mantis-vision.com>
2024-06-09 14:54:30 +0300

    skeleton of deshboard is working

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import requests
 
 app = Flask(__name__)
 
-
+@app.route('/')
 def home():
     return render_template('home.html')
 
-
+@app.route('/stats')
 def stats():
     stats_data = {
         'season': '2023-2024',
