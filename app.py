8d68380c5f036c1f6d75a19bae9f314db49a082e
or-mv <or.gamliel@mantis-vision.com>
2024-06-09 16:48:06 +0300

    fixed the stats issue

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
+import pandas as pd
+
 
 app = Flask(__name__)
 
 # Fetching Deni data
 player_dict = players.get_players()
-deni_avdija = [player for player in player_dict in players['full_name'] == 'Deni Avdija'][0]
+deni_avdija = [player for player in player_dict if player['full_name'] == 'Deni Avdija'][0]
 deni_avdija_id = deni_avdija['id']
 
 @app.route('/')
@@ -25,14 +27,16 @@ def stats():
     stats_data = {
         'season': latest_season['SEASON_ID'],
         'games_played': latest_season['GP'],
-        'points_per_game': latest_season['PTS'],
-        'rebounds_per_game': latest_season['REB'],
-        'assists_per_game': latest_season['AST'],
-        'field_goal_percentage': latest_season['FG_PCT'] * 100,
-        'three_point_percentage': latest_season['FG3_PCT'] * 100,
-        'free_throw_percentage': latest_season['FT_PCT'] * 100
+        'points_per_game': latest_season['PTS'] / latest_season['GP'],  # Points per game
+        'rebounds_per_game': latest_season['REB'] / latest_season['GP'],  # Rebounds per game
+        'assists_per_game': latest_season['AST'] / latest_season['GP'],  # Assists per game
+        'field_goal_percentage': latest_season['FG_PCT'] * 100,  # Field goal percentage
+        'three_point_percentage': latest_season['FG3_PCT'] * 100,  # Three-point percentage
+        'free_throw_percentage': latest_season['FT_PCT'] * 100  # Free throw percentage
     }
+
     return render_template('stats.html', stats=stats_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
